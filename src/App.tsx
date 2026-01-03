4b5e0bf46a96c3cc878436668bbf069155e102ef
lcizmarev21 <lchioffical@gmail.com>
2026-01-03 02:50:12 +0100

    Restart the background to make it with three.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import Launcher from './components/Launcher/Launcher'
-import Background from './components/Background/Background';
+
 
 function App() {
   return (
     <div>
-      <div >
-        <Background />
-      </div>
+      
 
       <div className="min-h-screen flex items-end pb-25 justify-center z-10">
           <Launcher />
