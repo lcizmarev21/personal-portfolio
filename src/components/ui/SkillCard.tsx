669300844437852804db60a1c59d356543c32016
lcizmarev21 <lchioffical@gmail.com>
2026-04-27 20:08:29 +0200

    Edited skillcard

--- src/components/ui/SkillCard.tsx
+++ src/components/ui/SkillCard.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function SkillCard({ skill }: Props) {
     return (
-        <div className="flex items-center gap-3 border border-[#D2D7D9] rounded-lg px-4 py-2">
+        <div className="flex items-center gap-3 border border-[#D2D7D9] rounded-lg px-4 py-2 bg-[#D2D7D9]/95 text-[#0B0D0D]">
 
             <img src={skill.icon} alt={`${skill.name} icon`} className="w-6 h-6" />
 
