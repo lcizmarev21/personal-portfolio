d74880ef47ab64fd7e3d483addb8d1319ecd686c
lcizmarev21 <lchioffical@gmail.com>
2026-05-07 19:26:59 +0200

    Fixed opacity

--- src/components/ui/ContactButton.tsx
+++ src/components/ui/ContactButton.tsx
@@ -9,7 +9,7 @@ export default function ContactButton({ contact }: Props) {
         <a
             href={contact.link}
             target="_blank"
-            className="items-center gap-2  duration-300 border-2 rounded-md px-4 py-4 border-[#D2D7D9] opacity-75 bg-[#D2D7D9] "
+            className="items-center gap-2  duration-300 border-2 rounded-md px-4 py-4 border-[#D2D7D9] bg-[#D2D7D9] "
         >
             <img src={contact.icon} alt={contact.label} className="w-10 h-10 m-2" />
         </a>
