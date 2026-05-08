cf4079828dee2563b608853cb888bf4a1068c2d3
lcizmarev21 <lchioffical@gmail.com>
2026-05-08 22:10:22 +0200

    Working on hero section animations

--- src/features/home/Hero.tsx
+++ src/features/home/Hero.tsx
@@ -1,13 +1,16 @@
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 export default function Hero() {
+
+    const [open,isOpen] = useState(false);
     return (
-        <section id="home" className="min-h-screen flex items-center px-8 scroll-mt-20">
+        <section id="home" className="min-h-screen flex items-center px-8 scroll-mt-20  ">
             <motion.div 
                 className="w-full flex flex-row justify-between items-center gap-6"
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1 }}
+                transition={{ duration: 1,  ease: "easeOut"}}
             >
 
                 {/* Left side */}
@@ -21,14 +24,38 @@ export default function Hero() {
                         clean and responsive web applications.
                     </h1>
 
-                    <motion.button 
-                        initial={{ opacity: 0, y: 20 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 1 }}
-                        className="mt-10 opacity-45 font-thin"
-                    >
-                        More ...
-                    </motion.button>
+                    <div>
+                        <motion.button 
+                            transition={{type:"spring" , stiffness:250, damping:15}}
+                            whileHover={{opacity:60, scale: 1.25 , y: -4 }}
+                            whileTap={{scale: 0.9, y: 0 }}
+                            className="mt-10 opacity-45 font-thin"
+                            onClick={() => 
+                                isOpen((o) => !o) 
+                            }
+                        >
+                            More ...
+                        </motion.button>
+                        <AnimatePresence>
+                            {open && (
+                                <motion.div id="more" className="mt-10 opacity-45 font-thin "
+                                    initial={{ opacity: 0, y:20 }}
+                                    animate={{ opacity: 1, y: 0 }}
+                                    exit={{opacity:0 , y:20}}
+                                    transition={{ duration: 0.5 }}
+                                >
+                                    <p>
+                                    I've been learning frontend development by myself for the past year, focusing on modern JavaScript frameworks and responsive design principles. <br /> <br /> 
+
+                                    Currently looking for an internship or junior position where I can gain real-world experience and continue to develop my skills. I'm eager to learn and collaborate with experienced professionals in the field.
+
+                                    </p>
+                                </motion.div>
+                            )}
+                        </AnimatePresence>
+                   
+                    </div>
+
                 </div>
 
 
