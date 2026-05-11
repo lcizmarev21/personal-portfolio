1c926ed36ff9e36abfc4b235ae1868fb6c1d8cb4
lcizmarev21 <lchioffical@gmail.com>
2026-05-11 15:36:02 +0200

    Fixed project card a bit

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -2,19 +2,19 @@ import type {Project} from "../../types/project.ts"
 
 export default function ProjectCard({project}: {project: Project }) {
     return (
-        <div className="flex flex-row justify-between items-center p-6 border rounded h-screen w-screen">
+        <div className="flex flex-row justify-between items-center  border rounded w-full h-full p-10 gap-30">
 
-            <div className="w-1/2 flex flex-col items-center ml-24">
-                <img src={project.image} alt={project.title} className="w-full h-full object-cover " />
+            <div className="w-1/2 flex justify-center ml-24">
+                <img src={project.image} alt={project.title} className="w-full h-full object-cover rounded-xl " />
             </div>
 
-            <div className="w-1/2 items-center flex flex-col">
+            <div className="w-1/2 justify-center flex flex-col">
                 <h3 className="text-3xl text-semibold">{project.title} 1 </h3>
-                <p className="mt-4"> {project.description} </p>
+                <p className="mt-4 opacity-75"> {project.description} </p>
 
-               <div className="flex gap-2 mt-4 flex-wrap">
+               <div className="flex gap-2 mt-6 flex-wrap">
                     {project.tags.map((tag) => (
-                        <span key={tag} className="text-xs border px-2 py-1 rounded">
+                        <span key={tag} className="text-xs border px-2 py-1 rounded-full opacity-75">
                             {tag}
                         </span>
                      ))}
