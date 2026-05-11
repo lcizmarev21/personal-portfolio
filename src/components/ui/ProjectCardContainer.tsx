7b4a65861ad5e186dd23e85506285331b4c4110f
lcizmarev21 <lchioffical@gmail.com>
2026-05-12 01:39:15 +0200

    Project card works , next is animating it

--- src/components/ui/ProjectCardContainer.tsx
+++ src/components/ui/ProjectCardContainer.tsx
@@ -3,7 +3,7 @@ import ProjectsCard from "../../components/ui/ProjectCard.tsx";
 
 export default function ProjectCardContainer() {
     return (
-         <div className="mt-30 flex flex-col gap-12 h-screen w-screen items-center justify-center">
+         <div className="mt-30 flex flex-col justify-between gap-25 items-center  ">
             {
                 projects.map((project, index) => {
                 return ( <ProjectsCard key={index} project={project}/> )
--- src/data/projects_data.ts
+++ src/data/projects_data.ts
@@ -10,13 +10,13 @@ export const projects: Project[] = [
   {
     title: "Plantly",
     description: "Webapp daily to do list with gamification elements and social features.",
-    image: "/project2.png",
+    image: "/project_images/cloud.jpg",
     tags: ["React", "Typescript", "Supabase" , "Framer Motion" , "Tailwind" , "Vercel"],
   },
   {
     title: "Swaether",
     description: "Mobile app that provides you with accurate weather forecasts and recommendations on what to wear based on the weather conditions.",
-    image: "/project3.png",
+    image: "/project_images/cloud.jpg",
     tags: ["React", "API"],
   }
 ];
