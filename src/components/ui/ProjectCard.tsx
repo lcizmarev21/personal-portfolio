31d5bdfd03f9eaa62aa715c4bf8f265c52b7cde5
lcizmarev21 <lchioffical@gmail.com>
2026-05-11 21:05:55 +0200

    Made cards sticky

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -2,19 +2,19 @@ import type {Project} from "../../types/project.ts"
 
 export default function ProjectCard({project}: {project: Project }) {
     return (
-        <div className="flex flex-row justify-between items-center w-full h-full gap-30  ">
+        <div className="flex flex-row justify-between items-center w-full h-full gap-30 sticky top-20">
 
-            <div className="w-1/2 flex justify-center ml-24 border-r p-12">
-                <img src={project.image} alt={project.title} className="w-full h-full object-cover rounded-xl " />
+            <div className="w-1/2 flex justify-center ml-24 border-r border-[#D2D7D9]/50 pr-12 ">
+                <img src={project.image} alt={project.title} className="w-full h-full object-fit rounded-3xl" />
             </div>
 
             <div className="w-1/2 justify-center flex flex-col">
-                <h3 className="text-3xl text-semibold">{project.title} 1 </h3>
-                <p className="mt-4 opacity-75"> {project.description} </p>
+                <h3 className="text-3xl text-semibold">{project.title} </h3>
+                <p className="mt-4 opacity-75 font-light"> {project.description} </p>
 
                <div className="flex gap-2 mt-6 flex-wrap">
                     {project.tags.map((tag) => (
-                        <span key={tag} className="text-xs border px-2 py-1 rounded-full opacity-75">
+                        <span key={tag} className="text-xs border px-3 py-1.5 rounded-full opacity-75 font-extralight">
                             {tag}
                         </span>
                      ))}
--- src/components/ui/ProjectCardContainer.tsx
+++ src/components/ui/ProjectCardContainer.tsx
@@ -3,12 +3,12 @@ import ProjectsCard from "../../components/ui/ProjectCard.tsx";
 
 export default function ProjectCardContainer() {
     return (
-         <div className="mt-12 flex flex-col gap-24 h-screen items-center justify-center sticky top-0">
-                            {
-                                projects.map((project, index) => {
-                                return ( <ProjectsCard key={index} project={project}/> )
-                                })
-                            }                       
-                            </div>
+         <div className="mt-12 flex flex-col gap-12 h-screen w-screen items-center justify-center">
+            {
+                projects.map((project, index) => {
+                return ( <ProjectsCard key={index} project={project}/> )
+                })
+            }                       
+        </div>
     );
 }   
