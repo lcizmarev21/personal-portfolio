4f8b1fb06145e3b18189bf4c1d55d58c647fb259
lcizmarev21 <lchioffical@gmail.com>
2026-05-15 18:57:50 +0200

    Added style to footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,8 +1,12 @@
 export default function Footer() {
     return (
-        <footer className="w-full py-10 flex items-center justify-center border-t border-white/10 text-[#D2D7D9] ">
-            <p className="text-sm">
-                © 2026 Leon Cizmarevic. All rights reserved.
+        <footer className="w-full py-10 flex items-center justify-between border-t border-white/10 text-[#D2D7D9] px-20 ">
+            <p className="text-sm font-extralight">
+                © 2026 Leon Cizmarevic.
+            </p>
+
+            <p className="text-sm font-extralight">
+                All rights reserved.
             </p>
         </footer>
     );
