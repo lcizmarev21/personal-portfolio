4882e498a4b9e7554b4ed6880987630562d9d0f9
lcizmarev21 <lchioffical@gmail.com>
2026-05-15 19:02:57 +0200

    Added lenis scroll

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import Contact from './features/contact/Contact.tsx';
 import Hero from './features/home/Hero.tsx';
 import Projects from './features/projects/Projects.tsx';
 import Skills from './features/skills/Skills.tsx';
+import SmoothScrolling from './components/providers/SmoothScrolling.tsx';
 
 function App() {
   return (
-    <Layout>
-      <Hero />
-      <Projects />
-      <Skills />
-      <Contact />
-    </Layout>
+    <SmoothScrolling >
+      <Layout>
+        <Hero />
+        <Projects />
+        <Skills />
+        <Contact />
+      </Layout>
+    </SmoothScrolling>
   );
 }
 
