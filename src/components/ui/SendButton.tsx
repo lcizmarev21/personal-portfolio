7e7c0e92796a9aa0df92dd51d2314aa2ad4bdb0e
lcizmarev21 <lchioffical@gmail.com>
2026-05-17 17:15:56 +0200

    Styled send button

--- src/components/ui/SendButton.tsx
+++ src/components/ui/SendButton.tsx
@@ -1,10 +1,15 @@
+import {motion} from "framer-motion";
+
 export default function SendButton(){
     return(
-        <button 
+        <motion.button 
         type="submit"
-        className="border border-[#D2D7D9] text-[#0B0D0D] bg-[#D2D7D9] py-2 px-4 w-40 rounded-full ml-65 hover:bg-[#0B0D0D] hover:text-[#D2D7D9]"
+        className="border-i-[#0B0D0D] border-[#D2D7D9] text-[#0B0D0D] bg-[#D2D7D9] py-2 px-4 w-40 rounded-full ml-65 font-thin"
+        transition={{type:"spring" , stiffness:250, damping:15}}
+        whileHover={{opacity:0.9, scale: 1.2 , y: -4 }}
+        whileTap={{scale: 0.9, y: 0 }}
         >
             Send
-         </button>
+         </motion.button>
     )
 }
